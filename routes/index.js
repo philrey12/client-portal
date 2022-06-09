182441b75b5148d1a874c7b4cadc79844f897f06
philrey12 <phil.raquinel@gmail.com>
2022-06-09 19:12:46 +0800

    Update libraries; add index.js and user.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+// home page
+router.get('/', (req, res) => {
+    res.send('Home Page')
+})
+
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+// home page
+router.get('/user', (req, res) => {
+    res.send('User Page')
+})
+
 module.exports = router
